1b14eb3be0366b329692d3e51612afcaa6490907
shenfu <xuanyuanshenfu@outlook.com>
2021-07-01 13:52:47 +0800

    .

--- Sources/App/Controllers/RootController.swift
+++ Sources/App/Controllers/RootController.swift
@@ -24,35 +24,35 @@ class RootController {
     }
     
     func query() {
-        mysqlDB.query("select * from shenfu") { row in
+       try? mysqlDB.query("select * from shenfu") { row in
             let value1 = row.column("name")?.string ?? ""
             let value2 = row.column("score")?.double ?? 0
             debugPrint("\(value1),\(value2)")
         } onMetadata: { _ in
-        }
+        }.wait()
     }
     
     func update() {
-        mysqlDB.query("update shenfu set score = ? where name = ?",[90,"张无忌"]) { row in
+        try? mysqlDB.query("update shenfu set score = ? where name = ?",[90,"张无忌"]) { row in
         } onMetadata: { _ in
-        }
+        }.wait()
     }
     
     func delete() {
-        mysqlDB.query("delete from shenfu where name = ?",["陆小凤"]) { row in
+        try? mysqlDB.query("delete from shenfu where name = ?",["陆小凤"]) { row in
         } onMetadata: { _ in
-        }
+        }.wait()
     }
     
     func add() {
-        mysqlDB.query("insert into shenfu VALUES (?,?)",["陆小凤",99]) { row in
+        try? mysqlDB.query("insert into shenfu VALUES (?,?)",["陆小凤",99]) { row in
         } onMetadata: { _ in
-        }
+        }.wait()
     }
     
     func add1() {
-        mysqlDB.query("insert into shenfu VALUES (?,?)",["张无忌",199]) { row in
+        try? mysqlDB.query("insert into shenfu VALUES (?,?)",["张无忌",199]) { row in
         } onMetadata: { _ in
-        }
+        }.wait()
     }
 }
