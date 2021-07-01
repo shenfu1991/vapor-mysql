f7a511cc811943cf66a4fbda96e5ea7299747306
shenfu <xuanyuanshenfu@outlook.com>
2021-07-01 13:57:38 +0800

    6

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -31,17 +31,29 @@ public func configure(_ app: Application) throws {
     let rows = try mysqlDB.simpleQuery("SELECT @@version;").wait()
     debugPrint(rows)
     
+    var arr: [String] = []
+    
     try? mysqlDB.query("select * from shenfu") { row in
         let value1 = row.column("name")?.string ?? ""
         let value2 = row.column("score")?.double ?? 0
         debugPrint("\(value1),\(value2)")
+        arr.append(value1)
     } onMetadata: { _ in
     }.wait()
-   
-
     
-        let vc = RootController()
-        vc.boot()
+
+    arr.forEach { name in
+        try? mysqlDB.query("update shenfu set score = ? where name = ?",[99,MySQLData(string: name)]) { row in
+            let value1 = row.column("name")?.string ?? ""
+            let value2 = row.column("score")?.double ?? 0
+            debugPrint("\(value1),\(value2)")
+            arr.append(value1)
+        } onMetadata: { _ in
+        }.wait()
+    }    
+    
+    let vc = RootController()
+    vc.boot()
   
  
 }
