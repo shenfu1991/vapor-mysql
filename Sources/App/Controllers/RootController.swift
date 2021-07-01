10f905b7e1c6176d1e0e06ca85643dd975614ee2
shenfu <xuanyuanshenfu@outlook.com>
2021-07-01 13:51:28 +0800

    update

--- Sources/App/Controllers/RootController.swift
+++ Sources/App/Controllers/RootController.swift
@@ -10,50 +10,49 @@ import Vapor
 import MySQLNIO
 
 class RootController {
-//    let db =  kApp.db  as! MySQLDatabase
 
-//    func boot() {
-//        debugPrint("boot")
-//        add()
-//        add1()
-//        DispatchQueue.global().asyncAfter(deadline: .now()+5) {
-//            self.update()
-//        }
-//        DispatchQueue.global().asyncAfter(deadline: .now()+7) {
-//            self.query()
-//        }
-//    }
-//    
-//    func query() {
-//        db.query("select * from shenfu") { row in
-//            let value1 = row.column("name")?.string ?? ""
-//            let value2 = row.column("score")?.double ?? 0
-//            debugPrint("\(value1),\(value2)")
-//        } onMetadata: { _ in
-//        }
-//    }
-//    
-//    func update() {
-//        db.query("update shenfu set score = ? where name = ?",[90,"张无忌"]) { row in
-//        } onMetadata: { _ in
-//        }
-//    }
-//    
-//    func delete() {
-//        db.query("delete from shenfu where name = ?",["陆小凤"]) { row in
-//        } onMetadata: { _ in
-//        }
-//    }
-//    
-//    func add() {
-//        db.query("insert into shenfu VALUES (?,?)",["陆小凤",99]) { row in
-//        } onMetadata: { _ in
-//        }
-//    }
-//    
-//    func add1() {
-//        db.query("insert into shenfu VALUES (?,?)",["张无忌",199]) { row in
-//        } onMetadata: { _ in
-//        }
-//    }
+    func boot() {
+        debugPrint("boot")
+        add()
+        add1()
+        DispatchQueue.global().asyncAfter(deadline: .now()+5) {
+            self.update()
+        }
+        DispatchQueue.global().asyncAfter(deadline: .now()+7) {
+            self.query()
+        }
+    }
+    
+    func query() {
+        mysqlDB.query("select * from shenfu") { row in
+            let value1 = row.column("name")?.string ?? ""
+            let value2 = row.column("score")?.double ?? 0
+            debugPrint("\(value1),\(value2)")
+        } onMetadata: { _ in
+        }
+    }
+    
+    func update() {
+        mysqlDB.query("update shenfu set score = ? where name = ?",[90,"张无忌"]) { row in
+        } onMetadata: { _ in
+        }
+    }
+    
+    func delete() {
+        mysqlDB.query("delete from shenfu where name = ?",["陆小凤"]) { row in
+        } onMetadata: { _ in
+        }
+    }
+    
+    func add() {
+        mysqlDB.query("insert into shenfu VALUES (?,?)",["陆小凤",99]) { row in
+        } onMetadata: { _ in
+        }
+    }
+    
+    func add1() {
+        mysqlDB.query("insert into shenfu VALUES (?,?)",["张无忌",199]) { row in
+        } onMetadata: { _ in
+        }
+    }
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,20 +4,12 @@ import FluentMySQLDriver
 import MySQLNIO
 
 var kApp: Application!
+var mysqlDB: MySQLDatabase!
+
 public func configure(_ app: Application) throws {
     
     kApp = app
-    
-//    app.databases.use(.mysql(
-//       hostname: "localhost",
-//       username: "root",
-//       password: "shenfu1991",
-//       database: "vapor",
-//        tlsConfiguration: .forClient(certificateVerification: .none)
-//     ), as: .mysql)
-//    try app.autoMigrate().wait()
-   try? routes(app)
-
+    try? routes(app)
     let configuration = MySQLConfiguration(
         hostname: "localhost",
         port: 3306,
@@ -34,20 +26,12 @@ public func configure(_ app: Application) throws {
         on: eventLoopGroup
     )
     defer { pools.shutdown() }
-//     app.migrations.add(CreateAcronym())
-
-//     app.logger.logLevel = .debug
-     
-//    pools.withConnection(logger: Logger(label: "meng"), on: eventLoopGroup.next()) { cnn in
-//
-//    }
-      
-   let pool = pools.pool(for: eventLoopGroup.next())
-    let mysql = pool.database(logger: Logger(label: "meng")) // MySQLDatabase
-    let rows = try mysql.simpleQuery("SELECT @@version;").wait()
+    let pool = pools.pool(for: eventLoopGroup.next())
+    mysqlDB = pool.database(logger: Logger(label: "meng")) // MySQLDatabase
+    let rows = try mysqlDB.simpleQuery("SELECT @@version;").wait()
     debugPrint(rows)
     
-    try? mysql.query("select * from shenfu") { row in
+    try? mysqlDB.query("select * from shenfu") { row in
         let value1 = row.column("name")?.string ?? ""
         let value2 = row.column("score")?.double ?? 0
         debugPrint("\(value1),\(value2)")
@@ -56,9 +40,8 @@ public func configure(_ app: Application) throws {
    
 
     
-//    DispatchQueue.global().asyncAfter(deadline: .now()+2) {
-//        let vc = RootController()
-//        vc.boot()
-//    }
+        let vc = RootController()
+        vc.boot()
+  
  
 }
